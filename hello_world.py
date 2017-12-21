327a12aeba57c595e6ebb179051c27ef63f4eef0
Qing-Yu-1 <l2830942138@gmail.com>
2017-12-21 11:59:14 +0800

    initial project version

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+print("hello python world")
+message = "Hello Python world!" 
+print(message) 
+message='I told my friend, "Python is my favorite language!"'
+print(message) 
+message1="The language 'Python' is named after Monty Python, not the snake."
+print(message1)
+#title()以首字母大写的方式显示每个单词，即将每个单词的首字母都改为大写
+#qy change this
+name = "ada loVelace" 
+print(name.title()) 
+
+print(name.upper()) 
+print(name.lower())
+
+first_name = "ada" 
+last_name = "lovelace" 
+full_name = first_name + " " + last_name
+print(full_name)
+
